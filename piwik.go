19e1f63eb2723ee127c7cd98ac98d01b858036bb
veecue <veecue@ventos.tk>
2017-10-25 18:42:54 +0200

    init

---
+++ piwik.go
@@ -0,0 +1,62 @@
+package piwik
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"gopkg.in/macaron.v1"
+)
+
+type Options struct {
+	PiwikUrl         string
+	IgnoreDoNotTrack bool
+	WebsiteID        string
+	Token            string
+}
+
+func prepareOptions(options []Options) Options {
+	var opt Options
+	if len(options) > 0 {
+		opt = options[0]
+	}
+	opt.PiwikUrl = strings.TrimSuffix(strings.TrimSuffix(opt.PiwikUrl, "piwik.php"), "/") + "/piwik.php?"
+
+	return opt
+}
+
+func Piwik(options ...Options) macaron.Handler {
+	opt := prepareOptions(options)
+
+	return func(ctx *macaron.Context, logger *log.Logger) {
+		params := make(url.Values)
+		params.Set("idSite", opt.WebsiteID)
+		params.Set("rec", "1")
+
+		params.Set("url", ctx.Req.URL.String())
+		params.Set("apiv", "1")
+
+		h := ctx.Header()
+		params.Set("urlref", h.Get("Referer"))
+		params.Set("ua", h.Get("User-Agent"))
+		params.Set("lang", h.Get("Accept-Language"))
+
+		params.Set("token_auth", opt.Token)
+		params.Set("cip", ctx.RemoteAddr())
+
+		// collecting data is finished, go async now
+		go func() {
+			logger.Println(params.Encode())
+			res, err := http.Get(opt.PiwikUrl + params.Encode())
+			if err != nil {
+				logger.Println("Error contacting piwik:", err)
+			}
+			r, err := ioutil.ReadAll(res.Body)
+			logger.Println(string(r))
+		}()
+
+		ctx.Next()
+	}
+}
